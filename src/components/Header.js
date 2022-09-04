c7661399f3bf6d9f97d1c8cdf8fe23ababe1475e
Junaid Malik <junmalik109@gmail.com>
2022-09-04 17:21:08 +0100

    updated component for Header

---
+++ src/components/Header.js
@@ -0,0 +1,101 @@
+import { Style } from '@material-ui/icons'
+import { styled } from '@material-ui/core'
+import { Avatar } from '@material-ui/core'
+import { AccessTimeIcon } from '@material-ui/icons'
+import { SearchIcon } from '@material-ui/icons/Search';
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+    <h1>Test</h1>
+    {/* <HeaderLeft> */}
+    <HeaderLeft>
+
+    {/* <HeaderAvatar> */}
+    <HeaderAvatar>
+    </HeaderAvatar>
+    <AccessTimeIcon></AccessTimeIcon>
+    </HeaderLeft>
+
+    {/* </HeaderSearch> */}
+    <HeaderSearch>
+        <SearchIcon>
+
+        </SearchIcon>
+    </HeaderSearch>
+
+    {/* <HeaderRight> */}
+    <HeaderRight></HeaderRight>
+
+    </HeaderContainer>  )
+}
+
+export default Header
+
+const HeaderContainer = styled.div`
+    display: flex;
+    color: "red";
+    position: fixed;
+    width: 100%;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px 0;
+    background-color: var(--slack-color);
+    color: white;
+    `
+
+const HeaderLeft = styled.div`
+    flex: 0.3;
+    display: flex;
+    align-items: center;
+    margin-left: 20px;
+    > h1 {
+        display: flex;
+        align-items: center;
+        margin-left: 20px;
+    }
+    > .muiSvgIcon-root {
+        margin-left: auto;
+        margin-right: 30px;
+    }
+
+`
+
+const HeaderSearch = styled.div`
+    flex: 0.4;
+    opacity: 1;
+    border-radius: 6px;
+    background-color: #421f44;
+    text-align: center;
+    display: flex;
+    padding: 0 50px;
+    color: gray;
+    border: 1px gray solid;
+    > input {
+        background-color: transparent;
+        border: none;
+        text-align: center;
+        min-width: 30vw;
+        outline: 0;
+        color: white;
+    }
+`
+
+const HeaderRight = styled.div`
+    flex: 0.3;
+    display: flex;
+    align-items: flex-end;
+    > p {
+        display: flex;
+        align-items: center;
+        font-size: 14px;
+    }
+`
+
+const HeaderAvatar = styled(Avatar)`
+    cursor: pointer;
+    :hover {
+        opacity: 0.8;
+    }
+`
+
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDFtKvP8zyeGDZ4gOglKK_KvWF3srRwBVA",
+    authDomain: "testproject12-818b3.firebaseapp.com",
+    databaseURL: "https://testproject12-818b3-default-rtdb.firebaseio.com",
+    projectId: "testproject12-818b3",
+    storageBucket: "testproject12-818b3.appspot.com",
+    messagingSenderId: "845291488949",
+    appId: "1:845291488949:web:7b9dded7f80e3347a1a0fc",
+    measurementId: "G-2WCF48107G"
+  };
